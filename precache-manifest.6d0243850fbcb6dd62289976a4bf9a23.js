7d890a19ae048022f4ab8ed08b5804dfd7014fd3
dannyTorres <dtorres@itexico.net>
2020-06-04 16:42:53 -0500

    Updates

---
+++ precache-manifest.6d0243850fbcb6dd62289976a4bf9a23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e3502ce6a374b4fa6ddb96726e5636",
+    "url": "/SnakeJS-React-Hooks/index.html"
+  },
+  {
+    "revision": "8886e4e3fa52f4b0b304",
+    "url": "/SnakeJS-React-Hooks/static/css/main.30881012.chunk.css"
+  },
+  {
+    "revision": "6a45054b8b85e1586a8a",
+    "url": "/SnakeJS-React-Hooks/static/js/2.0cd99a2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SnakeJS-React-Hooks/static/js/2.0cd99a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8886e4e3fa52f4b0b304",
+    "url": "/SnakeJS-React-Hooks/static/js/main.25bcfdad.chunk.js"
+  },
+  {
+    "revision": "1d0b7950545af6c78d58",
+    "url": "/SnakeJS-React-Hooks/static/js/runtime-main.7d2c3ad1.js"
+  }
+]);
